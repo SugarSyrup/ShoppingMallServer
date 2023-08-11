ae95d1b81029ce7732d7405204a4681e29921509
SugarSyrup <tlfvm04@naver.com>
2023-08-11 18:05:23 +0900

    FEAT : post features

--- models/post.js
+++ models/post.js
@@ -1,36 +1,42 @@
 const mongoose = require('mongoose');
 
+const contentSchema = {
+    text: String,
+    img: String
+}
+
 const postSchema = new mongoose.Schema({
-    id: {type: String, required:true, unique: true},
     title: {type: String, required:true},
     author : {type: String, required:true},
     email : {type: String, required:true},
-    content : { type: String, required: true},
+    password: {type: String, required:true},
+    explain : {type:String, required:true},
+    content : [contentSchema],
 },
 {
     timestamps: true
 });
 
+//Create
 postSchema.statics.create = function (payload) {
-    const user = new this(payload);
+    const post = new this(payload);
 
-    return user.save();
+    return post.save();
 }
 
-postSchema.statics.findAll = function () {
-    return this.find({});
+//Read
+postSchema.statics.findOne = function (email, password) {
+    return this.findOne({"email" : email, "password" : password});
 }
 
-postSchema.statics.findOneByUserid = function (userid) {
-    return this.findOne({ "id" : userid });
+//Update
+postSchema.statics.updateById = function (id, payload) {
+    return this.findOneAndUpdate({"id" : id}, payload, {new: true});    
 }
 
-postSchema.statics.updateByUserid = function (userid, payload) {
-    return this.findOneAndUpdate({ "id" : userid }, payload, {new : true});
-}
-
-postSchema.statics.deleteByUserid = function (userid) {
-    return this.deleteOne({ "id" : userid });
+//Delete
+postSchema.statics.deleteById = function (id) {
+    return this.deleteOne({ "id" : id });
 }
 
 module.exports = mongoose.model('post', postSchema);
--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,42 @@
 const router = require('express').Router();
 const imageUploader = require('../config/s3_config');
+const Post = require("../models/post");
 
-router.post('/test/image', imageUploader.single('image'), (req,res) => {
-    const filePath = req.file.location; //업로드된 이미지 경로
+//게시글 생성
+router.post('/create', (req, res) => {
+    const {title, author, password, email, explain, content} = req.body;
+    Post.create({title, author, email, explain, content})
+        .then((response) => {
+            res.status(200).json({"message" : "ok!"});
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        })
+})
+
+//게시글 읽기
+router.post("/", async(req, res) => {
+    const { email, password } = req.body;
+    Post.findOne(email, password)
+        .then((response) => {
+            if(!response) return res.status(404).json({ err: 'Post not found' });
+            res.status(200).json(response);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        })
+})
+
+//게시글 수정
+
+//게시글 삭제
+router.delete('/')
+
+
+
+router.post('/image', imageUploader.single('image'), (req,res) => {
+    const filePath = req.file.location; 
+
     if(!filePath) {
         throw new CustomError({
             status: 401,
@@ -11,8 +45,9 @@ router.post('/test/image', imageUploader.single('image'), (req,res) => {
             }
         })
     }
-    res.status(200).send(filePath);
-})
+
+    res.status(200).send({"path":filePath});
+});
 
 
 module.exports = router;
